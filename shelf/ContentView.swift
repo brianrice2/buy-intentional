a0963f4cf13a9691f94d8e8d30856e5c8cc9a031
Brian Rice <51218458+brianrice2@users.noreply.github.com>
2026-06-11 20:21:04 -0500

    Initial Commit

---
+++ shelf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shelf
+//
+//  Created by Brian Rice on 6/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shelf/shelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  shelfApp.swift
+//  shelf
+//
+//  Created by Brian Rice on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct shelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
